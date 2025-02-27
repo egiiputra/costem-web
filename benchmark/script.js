439ce8502e249ca93b349c0933130aa20feda3d4
egiiputra <egiputra450@gmail.com>
2025-02-27 22:59:15 +0700

    add benchmark script using k6

---
+++ benchmark/script.js
@@ -0,0 +1,12 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  vus: 10,
+  duration: '30s',
+};
+
+export default function () {
+  http.post(`${process.env.HOST_URL}/predicts`);
+  sleep(1);
+}
