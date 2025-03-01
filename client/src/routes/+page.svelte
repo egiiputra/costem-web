faccf60c2d6dbeda2eb90dcc03ba918f6a1838c9
egiiputra <egiputra450@gmail.com>
2025-03-01 10:50:50 +0700

    feat: Add table as outputs of inference

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,7 +1,45 @@
 <script lang="ts">
+	import { Fileupload, Label, Helper } from 'flowbite-svelte';
+	import {
+		Table,
+		TableBody,
+		TableBodyCell,
+		TableBodyRow,
+		TableHead,
+		TableHeadCell
+	} from 'flowbite-svelte';
+
+	interface Scores {
+		sehat: number;
+		giberella: number;
+		anthracnose: number;
+	}
+
+	interface Data {
+		filename: string;
+		scores: Scores | undefined;
+		output: string | undefined;
+	}
+
 	let files: FileList | undefined = $state();
 
+	let results: Data[] = $derived.by(() => {
+		if (files == undefined) return [];
+
+		let res = [];
+
+		for (const file of files) {
+			res.push({
+				filename: file.name,
+				scores: undefined,
+				output: undefined
+			});
+		}
+		return res;
+	});
+
 	$inspect(files);
+	$inspect(results);
 
 	function predicts() {
 		fetch('localhost:8000/predicts', {
@@ -14,6 +52,38 @@
 	}
 </script>
 
+<Label for="with_helper" class="pb-2">Upload file</Label>
+<Fileupload id="with_helper" class="mb-2" accept="image/jpeg,image/png" bind:files multiple />
+<Helper>PNG or JPG; MAX: 2MB</Helper>
+
+<Table>
+	<TableHead>
+		<TableHeadCell>Product name</TableHeadCell>
+		<TableHeadCell>Color</TableHeadCell>
+		<TableHeadCell>Category</TableHeadCell>
+		<TableHeadCell>Price</TableHeadCell>
+	</TableHead>
+	<TableBody tableBodyClass="divide-y">
+		<TableBodyRow>
+			<TableBodyCell>Apple MacBook Pro 17"</TableBodyCell>
+			<TableBodyCell>Sliver</TableBodyCell>
+			<TableBodyCell>Laptop</TableBodyCell>
+			<TableBodyCell>$2999</TableBodyCell>
+		</TableBodyRow>
+		<TableBodyRow>
+			<TableBodyCell>Microsoft Surface Pro</TableBodyCell>
+			<TableBodyCell>White</TableBodyCell>
+			<TableBodyCell>Laptop PC</TableBodyCell>
+			<TableBodyCell>$1999</TableBodyCell>
+		</TableBodyRow>
+		<TableBodyRow>
+			<TableBodyCell>Magic Mouse 2</TableBodyCell>
+			<TableBodyCell>Black</TableBodyCell>
+			<TableBodyCell>Accessories</TableBodyCell>
+			<TableBodyCell>$99</TableBodyCell>
+		</TableBodyRow>
+	</TableBody>
+</Table>
 <form action="" onsubmit={predicts}>
 	<input
 		id="files"
