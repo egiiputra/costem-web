b7c89f251a86c290598b9ae870c98d0ff5c6da04
egiiputra <egiputra450@gmail.com>
2025-03-18 23:17:43 +0700

    migrating to liteRT runtime for model inference

--- fastapi-tflite/main.py
+++ fastapi-tflite/main.py
@@ -1,30 +1,23 @@
-import json
-
-from typing import Union, List
+from typing import List
 from fastapi import (
     FastAPI, 
-    File, 
     UploadFile, 
     Response, 
     status
 )
 
-from tensorflow import (
-    convert_to_tensor,
-    uint8, 
-    float32
-)
-from tensorflow.io import decode_image
-from tensorflow.image import resize as image_resize
-import tensorflow.lite as tflite 
+import numpy as np
+import cv2
+from ai_edge_litert.interpreter import Interpreter
 
+BATCH_SIZE = 10
 
 CLASS_NAMES = ['sehat', 'giberella', 'anthracnose']
 
 MODEL_NAME = "vgg16-original-adam"
 
 # Load the TFLite model
-interpreter = tflite.Interpreter(model_path=f"models/{MODEL_NAME}.tflite")
+interpreter = Interpreter(model_path=f"models/{MODEL_NAME}.tflite")
 interpreter.allocate_tensors()  # Allocate memory for the model
 
 # Get input/output details
@@ -39,38 +32,34 @@ def read_root():
 
 @app.post("/predicts")
 async def predicts(images: List[UploadFile], response: Response):
+    if len(images) > BATCH_SIZE:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return { 'message' : 'To many images to inference' }
+
     # Check MIME Type make sure that file is image
     for image in images:
         if image.content_type not in ('image/jpeg', 'image/png'):
             response.status_code = status.HTTP_415_UNSUPPORTED_MEDIA_TYPE
             return { 'message' : 'Just can receive image file type'}
 
-    input_tensors = []
-
-    for i, image in enumerate(images):
-        # Get image data from body request
-        input_tensor = decode_image(
-            await image.read(),
-            channels=3,
-            dtype=uint8,
-            expand_animations=False
-        )
-        input_tensors.append(input_tensor)
-
     predictions = []
-    for i, input_tensor in enumerate(input_tensors):
-        input_tensor = image_resize(input_tensor, size=(224, 224), method='nearest')
+    for i, image in enumerate(images):
+        tmp = cv2.imdecode(
+            np.frombuffer(await image.read(), dtype=np.uint8),
+            cv2.IMREAD_COLOR_RGB
+        )
+        tmp = np.array(
+            object=[ cv2.resize(tmp, (224, 224)) / 255 ],
+            dtype=np.float32
+        )
 
-        input_tensor = convert_to_tensor([ input_tensor / 255 ], dtype=float32)
-
-        # Set input tensor
-        interpreter.set_tensor(input_details[0]['index'], input_tensor)
+        interpreter.set_tensor(input_details[0]['index'], tmp)
         # Run inference
         interpreter.invoke()
         # Get the output
         output_data = interpreter.get_tensor(output_details[0]['index'])
         output_data = output_data.tolist()
-        
+
         predictions.append({ 
             "file_name": images[i].filename,
             "scores": {clas:output_data[0][i] for i, clas in enumerate(CLASS_NAMES) }
